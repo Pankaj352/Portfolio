f4e38b68952137fa2195218190692b4b38732a40
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-01-03 12:11:08 +0530

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,20 +25,42 @@ const Footer = () => {
         </ul>
 
         <div className="footer__social">
-          <a href="https://www.linkedin.com/in/pankaj-maurya-7a9172228/" className="footer__social-icon" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.linkedin.com/in/pankaj-maurya-7a9172228/"
+            className="footer__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i class="bx bxl-linkedin"></i>
           </a>
-          <a href="https://github.com/Pankaj352" className="footer__social-icon" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/Pankaj352"
+            className="footer__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i class="bx bxl-github"></i>
           </a>
-          <a href="https://web.whatsapp.com/" className="footer__social-icon" target="_blank" rel="noreferrer">
+          <a
+            href="https://web.whatsapp.com/"
+            className="footer__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i class="bx bxl-whatsapp"></i>
           </a>
-          <a href="https://www.instagram.com/pankajmaurya352/" className="footer__social-icon" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.instagram.com/pankaj_maurya_352/"
+            className="footer__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i class="bx bxl-instagram"></i>
           </a>
         </div>
-        <span className="footer__copy">&copy;Pankaj Maurya. All rights reserved</span>
+        <span className="footer__copy">
+          &copy;Pankaj Maurya. All rights reserved
+        </span>
       </div>
     </footer>
   );
